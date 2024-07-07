07160761d634bf8d139b16bab35fe73cdfbc1aaa
HyunwooYi <83715335+HyunwooYi@users.noreply.github.com>
2024-07-07 14:00:58 +0900

    refactor: Follow 로직 리팩토링

--- src/main/java/com/example/satto/domain/follow/service/Impl/FollowServiceImpl.java
+++ src/main/java/com/example/satto/domain/follow/service/Impl/FollowServiceImpl.java
@@ -23,89 +23,52 @@ public class FollowServiceImpl implements FollowService {
 
     @Override
     public BaseResponse<Object> followRequest(String followingId, String studentId) {
-        if (followRepository.existsByFollowerIdStudentIdAndFollowingIdStudentId(studentId, followingId) &&
-                followRepository.existsByFollowerIdStudentIdAndFollowingIdStudentIdAndRequest(studentId, followingId, 1)) {
+        if (isFollowRequestAlreadySent(studentId, followingId)) {
             return BaseResponse.onSuccess("이미 Follow 요청을 보냈습니다.");
-        } else {
-            // Follower 사용자 조회
-            Optional<Users> followerUser = usersRepository.findByStudentId(studentId);
-            if (followerUser.isEmpty()) {
-                return BaseResponse.onFailure("Follower 사용자를 찾을 수 없습니다.");
-            }
-
-            // Following 사용자 조회
-            Optional<Users> followingUser = usersRepository.findByStudentId(followingId);
-            if (followingUser.isEmpty()) {
-                return BaseResponse.onFailure("Following 사용자를 찾을 수 없습니다.");
-            }
-
-            // Optional에서 실제 Users 객체를 꺼냄
-            Users Op_followerUser = followerUser.get();
-            Users Op_followingUser = followingUser.get();
-
-
-            // Follow 엔티티 생성 및 저장
-            Follow follow = new Follow();
-
-            follow.setFollowerId(Op_followerUser);
-            follow.setFollowingId(Op_followingUser);
-            follow.setRequest(1);
-            followRepository.save(follow);
-
         }
+        // 팔로워 조회
+        Users followerUser = findUserByStudentId(studentId);
+        // 팔로잉 조회
+        Users followingUser = findUserByStudentId(followingId);
+        Follow follow = createFollow(followerUser, followingUser);
+        followRepository.save(follow);
+
         return BaseResponse.onSuccess("Follow 요청을 보냈습니다.");
     }
 
     @Override
     public Map<String, String> followRequestList(String studentId) {
-        // 1. 팔로우 요청을 보낸 사람들의 목록을 가져온다
+        // 팔로우 요청을 보낸 사람들의 목록을 가져온다.
         List<Follow> followerRequests = followRepository.findByFollowingIdStudentIdAndRequest(studentId, 1);
-//        System.out.println("팔로우 요청 목록 크기: " + followerRequests.size());
-
         Map<String, String> followRequesters = new HashMap<>();
-        List<String> candidateStudentId = new ArrayList<>();
 
-        for (Follow candidate1 : followerRequests) {
-            candidateStudentId.add(candidate1.toString());
+        for (Follow follow : followerRequests) {
+            Users user = follow.getFollowerId();
+            followRequesters.put(user.getStudentId(), user.getName());
         }
 
-        for (String student : candidateStudentId) {
-            Optional<Users> optionalUser = usersRepository.findByStudentId(student);
-            if(optionalUser.isPresent()) {
-                Users user = optionalUser.get();
-                followRequesters.put(student, user.getName());
-
-            } else {
-                throw new UsersHandler(ErrorStatus._NOT_FOUND_USER);
-            }
-        }
         return followRequesters;
     }
 
     @Transactional
     @Override
     public BaseResponse<Object> acceptFollower(String followerId, String studentId) {
+        Follow followRequest = findFollowRequest(followerId, studentId);
 
-        Follow followerRequest = followRepository.findByFollowerIdStudentIdAndFollowingIdStudentIdAndRequest(followerId, studentId, 1);
-        boolean followerRequestExists = followRepository.existsByFollowerIdStudentIdAndFollowingIdStudentIdAndRequest(followerId, studentId, 1);
-
-        if (followerRequestExists) {
-            followerRequest.setRequest(2);
-            followRepository.save(followerRequest);
+        if (followRequest != null) {
+            followRequest.setRequest(2);
+            followRepository.save(followRequest);
             return BaseResponse.onSuccess("Follow 요청을 수락하였습니다.");
         } else {
             return BaseResponse.onFailure("Follow 요청 내역이 없습니다.");
         }
-
     }
 
     @Override
     public BaseResponse<Object> unFollower(String followerId, String studentId) {
+        Follow follow = findFollow(followerId, studentId);
 
-        Follow follow = followRepository.findByFollowerIdStudentIdAndFollowingIdStudentId(followerId, studentId);
-        boolean followerExist = followRepository.existsByFollowerIdStudentIdAndFollowingIdStudentId(followerId, studentId);
-
-        if (followerExist) {
+        if (follow != null) {
             followRepository.delete(follow);
             return BaseResponse.onSuccess("해당 Follower를 삭제하였습니다.");
         } else {
@@ -115,10 +78,9 @@ public class FollowServiceImpl implements FollowService {
 
     @Override
     public BaseResponse<Object> unFollowing(String followingId, String studentId) {
-        Follow follow = followRepository.findByFollowerIdStudentIdAndFollowingIdStudentId(studentId, followingId);
-        boolean followingExist = followRepository.existsByFollowerIdStudentIdAndFollowingIdStudentId(studentId, followingId);
+        Follow follow = findFollow(studentId, followingId);
 
-        if (followingExist) {
+        if (follow != null) {
             followRepository.delete(follow);
             return BaseResponse.onSuccess("해당 Following을 삭제하였습니다.");
         } else {
@@ -126,4 +88,29 @@ public class FollowServiceImpl implements FollowService {
         }
     }
 
+    private boolean isFollowRequestAlreadySent(String studentId, String followingId) {
+        return followRepository.existsByFollowerIdStudentIdAndFollowingIdStudentId(studentId, followingId) &&
+                followRepository.existsByFollowerIdStudentIdAndFollowingIdStudentIdAndRequest(studentId, followingId, 1);
+    }
+
+    private Users findUserByStudentId(String studentId) {
+        return usersRepository.findByStudentId(studentId)
+                .orElseThrow(() -> new UsersHandler(ErrorStatus._NOT_FOUND_USER));
+    }
+
+    private Follow createFollow(Users followerUser, Users followingUser) {
+        Follow follow = new Follow();
+        follow.setFollowerId(followerUser);
+        follow.setFollowingId(followingUser);
+        follow.setRequest(1);
+        return follow;
+    }
+
+    private Follow findFollowRequest(String followerId, String studentId) {
+        return followRepository.findByFollowerIdStudentIdAndFollowingIdStudentIdAndRequest(followerId, studentId, 1);
+    }
+
+    private Follow findFollow(String followerId, String followingId) {
+        return followRepository.findByFollowerIdStudentIdAndFollowingIdStudentId(followerId, followingId);
+    }
 }
