f5b5ca8755b05ca03dda1b03325860727fc4f094
chamm99 <alslalsl3125@gmail.com>
2024-08-02 10:55:49 +0900

    :sparkles: feat: EmailConfig classpath 어노테이션 삭제 - classpath 지정 시 프로퍼티 파일 변경 시마다 오류 발생

--- src/main/java/com/example/satto/config/EmailConfig.java
+++ src/main/java/com/example/satto/config/EmailConfig.java
@@ -3,16 +3,14 @@ package com.example.satto.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-
 @Configuration
-@PropertySource("classpath:application-dev.yml")
 public class EmailConfig {
+
     @Value("${spring.mail.host}")
     private String host;
 
